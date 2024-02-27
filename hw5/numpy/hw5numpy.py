674a7e24bdfd8cbc017408e4161797a16e047747
Zach Laurence <zachlaurence@Zach-MacBook-Pro-2.local>
2024-02-26 20:00:17 -0600

    numpy

--- hw5/numpy/hw5numpy.py
+++ hw5/numpy/hw5numpy.py
@@ -58,7 +58,9 @@ class NDArray1(NDArray):
         """
         Construct an NDArray1 from a list of int.
         """
-        raise NotImplementedError
+        self._data = data
+        self._shape = len(data)
+        
 
     def __add__(self, other: Any) -> "NDArray1":
         """
@@ -68,40 +70,53 @@ class NDArray1(NDArray):
         Raises Value error if other is neither an int nor
         and NDArray1, or an NDArray1 of different shape.
         """
-        raise NotImplementedError
+        if isinstance(other, int):
+            return NDArray1([x + other for x in self._data])
+        elif isinstance(other, NDArray1) and self.shape == other.shape:
+            result = []
+            for i in range(len(self._data)):
+                result.append(self._data[i] + other._data[i])
+            return NDArray1(result)
+        else:
+            raise ValueError("Wrong Shape")
 
     def __gt__(self, other: Any) -> list[bool]:
         """
         Return a list of bools indicating greater than given int.
         """
-        raise NotImplementedError
+        if not isinstance(other, int):
+            raise ValueError("Must be an int.")
+        return [x > other for x in self._data]
+
 
     def __contains__(self, other: Any) -> bool:
         """
         Test whether the given int is in the array.
         """
-        raise NotImplementedError
+        return other in self._data
 
     def __eq__(self, other: Any) -> bool:
         """
         Test whether other is an NDArray1 with the same shape and
         containing the same numbers.
         """
-        raise NotImplementedError
+        if isinstance(other, NDArray1) and self.shape == other.shape:
+            return self._data == other._data
+        return False
 
     @property
     def shape(self) -> Shape:
         """
         see NDArray
         """
-        raise NotImplementedError
+        return self._shape
 
     @property
     def data(self) -> list[int] | list[list[int]]:
         """
         see NDArray
         """
-        raise NotImplementedError
+        return self._data[:]
 
     @property
     def T(self) -> NDArray:
@@ -110,7 +125,7 @@ class NDArray1(NDArray):
 
         Note the transpose of a size-n 1D array is an nx1 2D array.
         """
-        raise NotImplementedError
+        return NDArray2([[x] for x in self._data])
 
 
 class NDArray2(NDArray):
@@ -128,7 +143,10 @@ class NDArray2(NDArray):
         Raises ValueError if list of lists is jagged (not
         rectangular).
         """
-        raise NotImplementedError
+        if any(len(row) != len(data[0]) for row in data):
+            raise ValueError("Array must be rectangular.")
+        self._data = data
+        self._shape = (len(data), len(data[0]))
 
     def __add__(self, other: Any) -> "NDArray2":
         """
@@ -138,41 +156,55 @@ class NDArray2(NDArray):
         Raises Value error if other is neither and int nor an
         NDArray2, or an NDArray2 of different shape.
         """
-        raise NotImplementedError
+        if isinstance(other, int):
+            return NDArray2([[x + other for x in row] for row in self._data])
+        elif isinstance(other, NDArray2) and self.shape == other.shape:
+            result = []
+            for i in range(len(self._data)):
+                row = []
+                for j in range(len(self._data[i])):
+                    row.append(self._data[i][j] + other._data[i][j])
+                result.append(row)
+            return NDArray2(result)
+        else:
+            raise ValueError("Operand must be an int or NDArray2 of the same shape.")
 
     def __gt__(self, other: Any) -> list[list[bool]]:
         """
         Return a list of lists of flags indicating greater than
         given int.
         """
-        raise NotImplementedError
+        if not isinstance(other, int):
+            raise ValueError("Operand must be an int.")
+        return [[x > other for x in row] for row in self._data]
 
     def __contains__(self, other: Any) -> bool:
         """
         Test whether the given int is anywhere in the array.
         """
-        raise NotImplementedError
+        return any(other in row for row in self._data)
 
     def __eq__(self, other: Any) -> bool:
         """
         Test whether other is an NDArray2 with the same shape and
         containing the same numbers.
         """
-        raise NotImplementedError
-
+        if isinstance(other, NDArray2) and self.shape == other.shape:
+            return self._data == other._data
+        return False
     @property
     def shape(self) -> Shape:
         """
         see NDArray
         """
-        raise NotImplementedError
+        return self._shape
 
     @property
     def data(self) -> list[int] | list[list[int]]:
         """
         see NDArray
         """
-        raise NotImplementedError
+        return [row[:] for row in self._data]
 
     @property
     def T(self) -> NDArray:
@@ -181,4 +213,12 @@ class NDArray2(NDArray):
 
         Note the transpose of an nx1 2D array is a size-n 1D array.
         """
-        raise NotImplementedError
+        num_cols = len(self._data[0])
+        transposed_data = [[] for _ in range(num_cols)]
+        for row in self._data:
+            for j, elem in enumerate(row):
+                transposed_data[j].append(elem)
+        if len(transposed_data) == 1:
+            return NDArray1(transposed_data[0])
+        else:
+            return NDArray2(transposed_data)
